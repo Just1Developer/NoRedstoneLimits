d4d5f780e45d8d8c7821ea83517788bd38b4e5d0
Just1Developer <weisserbenjamin98@gmail.com>
2024-07-19 15:51:43 +0200

    hmmmm

--- src/main/java/net/justonedev/mc/noRedstoneLimits/NoRedstoneLimits.java
+++ src/main/java/net/justonedev/mc/noRedstoneLimits/NoRedstoneLimits.java
@@ -5,7 +5,9 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.AnaloguePowerable;
 import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.type.RedstoneWire;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
@@ -34,19 +36,6 @@ public final class NoRedstoneLimits extends JavaPlugin implements Listener {
         instance = this;
         Bukkit.getPluginManager().registerEvents(this, this);
         getCommand("change").setExecutor(this);
-
-        /*
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            for (int i = 0; i < locs.size(); ++i) {
-                Location loc = locs.get(i);
-                Block block = loc.getBlock();
-                AnaloguePowerable powerable = (AnaloguePowerable) block.getBlockData();
-                powerable.setPower(MAX_CURRENT);
-                block.setBlockData(powerable);
-            }
-            Bukkit.broadcastMessage("§ePulse for §c" + blocks.size() + " §eblocks.");
-        }, 0, 60);
-         */
     }
 
     @Override
@@ -60,7 +49,6 @@ public final class NoRedstoneLimits extends JavaPlugin implements Listener {
     @EventHandler
     public void onRedstone(BlockRedstoneEvent e) {
         if (e.getBlock().getType() != Material.REDSTONE_WIRE) return;
-        //Bukkit.broadcastMessage("§eChange: " + e.getOldCurrent() + " -> " + e.getNewCurrent());
 
         if (!_do) return;
 
@@ -74,46 +62,36 @@ public final class NoRedstoneLimits extends JavaPlugin implements Listener {
         * */
 
         Block b = e.getBlock();
-        Block neighbor = b.getRelative(BlockFace.NORTH);
-        if (isRepeater(neighbor) && isPowered(neighbor, BlockFace.SOUTH)) {
+        if (isAnyNeighborActiveWatchingRepeater(b)) {
             e.setNewCurrent(MAX_CURRENT);
         }
+    }
 
-        neighbor = b.getRelative(BlockFace.EAST);
-        if (isRepeater(neighbor) && isPowered(neighbor, BlockFace.WEST)) {
-            e.setNewCurrent(MAX_CURRENT);
+    private static boolean isAnyNeighborActiveWatchingRepeater(Block block) {
+        Block neighbor = block.getRelative(BlockFace.NORTH);
+        Bukkit.broadcastMessage("§eNorth | isRepeater: §c" + isRepeater(neighbor) + "§e, isPowering: §c" + isPowering(neighbor, BlockFace.SOUTH) + "§e, isPowered: §c" + isPowered(neighbor));
+        if (isRepeater(neighbor) && isPowering(neighbor, BlockFace.SOUTH)) {
+            return true;
         }
 
-        neighbor = b.getRelative(BlockFace.SOUTH);
-        if (isRepeater(neighbor) && isPowered(neighbor, BlockFace.NORTH)) {
-            e.setNewCurrent(MAX_CURRENT);
+        neighbor = block.getRelative(BlockFace.EAST);
+        Bukkit.broadcastMessage("§dEast | isRepeater: §c" + isRepeater(neighbor) + "§d, isPowering: §c" + isPowering(neighbor, BlockFace.WEST) + "§d, isPowered: §c" + isPowered(neighbor));
+        if (isRepeater(neighbor) && isPowering(neighbor, BlockFace.WEST)) {
+            return true;
         }
 
-        neighbor = b.getRelative(BlockFace.WEST);
-        if (isRepeater(neighbor) && isPowered(neighbor, BlockFace.EAST)) {
-            e.setNewCurrent(MAX_CURRENT);
+        neighbor = block.getRelative(BlockFace.SOUTH);
+        Bukkit.broadcastMessage("§aSouth | isRepeater: §c" + isRepeater(neighbor) + "§a, isPowering: §c" + isPowering(neighbor, BlockFace.NORTH) + "§a, isPowered: §c" + isPowered(neighbor));
+        if (isRepeater(neighbor) && isPowering(neighbor, BlockFace.NORTH)) {
+            return true;
         }
 
-        /*
-        e.setNewCurrent(e.getOldCurrent());
-
-        final Block block = e.getBlock();
-        blocks.add(block);
-        if (!locs.contains(block.getLocation())) return;
-        locs.add(block.getLocation());
-         */
-        /*
-        final AnaloguePowerable powerable = (AnaloguePowerable) block.getBlockData();
-
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            //if (e.getNewCurrent() <= e.getOldCurrent()) return; // turning off or same
-            //e.setNewCurrent(MAX_CURRENT);    // turning on, set to full power
-            powerable.setPower(MAX_CURRENT);
-            block.setBlockData(powerable);
-            block.getState().update();
-        }, 0, 60);
-
-         */
+        neighbor = block.getRelative(BlockFace.WEST);
+        Bukkit.broadcastMessage("§bWest | isRepeater: §c" + isRepeater(neighbor) + "§b, isPowering: §c" + isPowering(neighbor, BlockFace.EAST) + "§b, isPowered: §c" + isPowered(neighbor));
+        if (isRepeater(neighbor) && isPowering(neighbor, BlockFace.EAST)) {
+            return true;
+        }
+        return false;
     }
 
     private static final Material REPEATER_MAT = Material.JACK_O_LANTERN;
@@ -126,36 +104,60 @@ public final class NoRedstoneLimits extends JavaPlugin implements Listener {
         reverseFace.put(BlockFace.WEST, BlockFace.EAST);
     }
 
-    private boolean isRepeater(Block noDelayRepeater) {
+    private static boolean isRepeater(Block noDelayRepeater) {
         if (noDelayRepeater == null) return false;
         if (noDelayRepeater.getType() != REPEATER_MAT) return false;
         return true;
     }
 
-    private boolean isPowered(Block noDelayRepeater) {
-        return isPowered(noDelayRepeater, BlockFace.SELF);
+    private static boolean isPowered(Block noDelayRepeater) {
+        return isPowering(noDelayRepeater, BlockFace.SELF);
     }
-    private boolean isPowered(Block noDelayRepeater, BlockFace isLookingTowards) {
+    private static boolean isPowering(Block noDelayRepeater, BlockFace isLookingTowards) {
         if (noDelayRepeater == null) return false;
+        Bukkit.broadcastMessage("0");
         if (noDelayRepeater.getType() != REPEATER_MAT) return false;
+        Bukkit.broadcastMessage("1");
 
         Directional directional = (Directional) noDelayRepeater.getBlockData();
+        Bukkit.broadcastMessage("2");
         BlockFace side = directional.getFacing();
         if (!reverseFace.containsKey(side)) return false;
+        Bukkit.broadcastMessage("3 + " + side);
+
+        // Todo hmmm
 
         Block block = noDelayRepeater.getRelative(side);
+        Bukkit.broadcastMessage("4 + " + reverseFace.get(side) + " + " + block.isBlockFacePowered(reverseFace.get(side)));
         return block.isBlockFacePowered(reverseFace.get(side))
                 && (isLookingTowards == BlockFace.SELF || isLookingTowards == side);    // Either self (default) or direction we want
     }
 
     @EventHandler
     public void onPlace(BlockPlaceEvent e) {
+        Block b = e.getBlock();
+        Bukkit.broadcastMessage("1");
+        if (b.getBlockData() instanceof AnaloguePowerable powerable) {
+            Bukkit.broadcastMessage("2");
+            if (!isAnyNeighborActiveWatchingRepeater(b)) return;
+            Bukkit.broadcastMessage("3");
+            powerable.setPower(powerable.getMaximumPower());
+            b.setBlockData(powerable);
+        }
+
         if (!isRepeater(e.getBlock())) return;
+
+        // Doesn't matter if it's powered or not
+        // If it isn't the power is gone so fast you don't even know it's there
+        // Also, if we do check, it hasn't properly updated yet so the last check in isPowered doesn't work
 
         Directional directional = (Directional) e.getBlock().getBlockData();
         BlockFace side = directional.getFacing();
         Block block = e.getBlock().getRelative(side);
-        block.setType(Material.EMERALD_BLOCK);
+        if (block.getBlockData() instanceof RedstoneWire redstoneWire) {
+            redstoneWire.setPower(redstoneWire.getMaximumPower());
+            block.setBlockData(redstoneWire);
+        }
     }
 
     @Override
